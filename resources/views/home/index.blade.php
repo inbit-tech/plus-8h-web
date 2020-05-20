fd6913b7750aeace4131d53d1860cef79cddb4f8
Vance Yao <330359531@qq.com>
2020-05-21 00:25:16 +0800

    add layout folder

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,10 @@
+@extends ('master')
+
+@section ('page-title')
+<title>@lang('navi.title')</title>
+@endsection
+
+@section('content')
+<p>213</p>
+
+@endsection
---
+++ resources/views/layout/common/header.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+{{-- <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"> --}}
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        @yield ('page-title')
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body is-family-sans-serif>
